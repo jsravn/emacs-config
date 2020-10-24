360c394b3eb59bfe91734ac65826843127f4e35e
James Ravn <james@r-vn.org>
2020-10-24 12:21:50 +0100

    Various small tweaks

--- packages.el
+++ packages.el
@@ -1,35 +1,15 @@
-;; [[file:/tmp/config.org.kw9qum::*Packages][Packages:1]]
+;; [[file:../../../../tmp/config.org.F26TNI::*Custom Packages][Custom Packages:1]]
 ;; -*- no-byte-compile: t; -*-
-;; Packages:1 ends here
+;; Custom Packages:1 ends here
 
-;; [[file:/tmp/config.org.kw9qum::*atomic-chrome][atomic-chrome:1]]
-(package! atomic-chrome)
-;; atomic-chrome:1 ends here
-
-;; [[file:/tmp/config.org.kw9qum::*org-roam][org-roam:1]]
-(package! org-roam :pin "4af4d2e4d5f18422fe0f90d5d83fe4c10ef1a20f")
-;; org-roam:1 ends here
-
-;; [[file:/tmp/config.org.kw9qum::*lsp-mode][lsp-mode:1]]
-(unpin! lsp-mode)
-;; lsp-mode:1 ends here
-
-;; [[file:/tmp/config.org.kw9qum::*mu4e-alert][mu4e-alert:1]]
-;(package! mu4e-alert)
-;; mu4e-alert:1 ends here
-
-;; [[file:/tmp/config.org.kw9qum::*org-msg][org-msg:1]]
-(package! org-msg)
-;; org-msg:1 ends here
-
-;; [[file:/tmp/config.org.kw9qum::*org-alert][org-alert:1]]
+;; [[file:../../../../tmp/config.org.F26TNI::*Package loading][Package loading:1]]
+;; custom packages
+(package! atomic-chrome)    ;; integration with the atomic chrome extension
 (package! org-alert)
-;; org-alert:1 ends here
-
-;; [[file:/tmp/config.org.kw9qum::*caddyfile-mode][caddyfile-mode:1]]
 (package! caddyfile-mode)
-;; caddyfile-mode:1 ends here
-
-;; [[file:/tmp/config.org.kw9qum::*protobuf-mode][protobuf-mode:1]]
 (package! protobuf-mode)
-;; protobuf-mode:1 ends here
+
+;; unpin these packages to use the latest always
+(unpin! org-roam)
+(unpin! lsp-mode)
+;; Package loading:1 ends here
